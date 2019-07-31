5b97e7b867ee1b4289d58a1c16d76f77d4a04007
Sam Finch <roadrunnersf@gmail.com>
2019-07-31 11:11:22 +0100

    implemented scoring logic using arrMatches function.

--- src/Guess/Score.js
+++ src/Guess/Score.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { arrMatches } from "./../Functions/index.js";
 import { Progress } from "reactstrap";
 
-const Score = ({ total, max }) => (
-  <div>
-    <div className="text-center">
-      {total || max} of {max}
+const Score = ({ total, max, scorer }) => {
+  return (
+    <div>
+      <div className="text-center">
+        {arrMatches(scorer)} of {max}
+      </div>
+      <Progress animated color="success" value={total || max} max={max} />
     </div>
-    <Progress animated color="success" value={total || max} max={max} />
-  </div>
-);
-
+  );
+};
 export default Score;
