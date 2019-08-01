fc8f9cf2d8e952a41a246d4b4d8451a09028e566
Sam Finch <roadrunnersf@gmail.com>
2019-08-01 12:02:26 +0100

    added logic to show number of correct / incorrect matches in progress bar and styled progress bar

--- src/Guess/Score.js
+++ src/Guess/Score.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { arrMatches } from "./../Functions/index.js";
+import { arrMatches, arrWrong } from "./../Functions/index.js";
 import { Progress } from "reactstrap";
 
 const Score = ({ total, max, scorer }) => {
+  const score = arrMatches(scorer);
+  const wrong = arrWrong(scorer);
   return (
     <div>
       <div className="text-center">
-        {arrMatches(scorer)} of {max}
+        {score} of {max}
       </div>
-      <Progress animated color="success" value={total || max} max={max} />
+      <Progress animated multi max={max} style={{ height: 40 }}>
+        <Progress
+          animated
+          bar
+          color="success"
+          value={score}
+          max={max}
+        ></Progress>
+        <Progress
+          animated
+          bar
+          color="danger"
+          value={wrong}
+          max={max}
+        ></Progress>
+      </Progress>
     </div>
   );
 };
